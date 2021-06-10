b271a9df17dcb7142e692be0f44ca818c2651908
Dirk Hoffmann <hoffi.mail@gmail.com>
2021-06-10 20:45:13 +0200

    INV as ini not yaml as AWX cannot deal with it

--- debug.sh
+++ debug.sh
@@ -3,6 +3,6 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${SCRIPTDIR}" || exit
-cmd='ansible-playbook -i debugINV.yml debug.yml -vv --limit="localhost"'
+cmd='ansible-playbook -i debugINV.ini debug.yml -vv --limit="localhost"'
 echo "${cmd}"
 eval ${cmd}
