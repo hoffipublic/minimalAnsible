fde4ecafc266a3a7843ce00513bd2fe626638b91
Dirk Hoffmann <hoffi.mail@gmail.com>
2021-06-16 10:42:03 +0200

    enhanced documentation

--- debug.sh
+++ debug.sh
@@ -3,6 +3,7 @@
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "${SCRIPTDIR}" || exit
+# --vault-password-file ./secrets/vaultSecret.txt
 cmd='ansible-playbook -i debugINV.ini debug.yml -vv --limit="localhost"'
 echo "${cmd}"
 eval ${cmd}
--- scripts/awxRunJobTemplate.sh
+++ scripts/awxRunJobTemplate.sh
@@ -5,25 +5,18 @@ SCRATCH_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 
 set -e # exit on error
 >&2 echo "read shared bashlibs .."
-source "${SCRATCH_SCRIPTDIR}/jenkins/jenkinsbashlib.sh"
 source "${SCRATCH_SCRIPTDIR}/awx/awxbashlib.sh"
 >&2 echo -e "reading shared bashlibs done.\n"
 
 
 awxuser=awx
 awxpassword=awx
-jenkinsuser=hoffi
 
 
-# token JViTPB7aoUysBVJ2Jb3dsf6rjkGWz2
 awxtoken="JViTPB7aoUysBVJ2Jb3dsf6rjkGWz2"
 # awxrefre jEIJAR5KO9fBGicv5LhQEP76XRVOYY
-jenkinstoken="11f4156bed98239a76a889a50e6df1e7d3" # hoffitoken
 
 set -x
-
-# jenkins_curlPOSTWithTokenWithoutData $jenkinstoken "job/hoffi/job/minimalAnsible/build"
-
 
 # curl -L -s -S -k -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer ${awxtoken}" \
 #   ${HTTPS_Jenkins}/api/v2/job_templates/hoffijob/launch/
