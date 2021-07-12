761b60fa8a9a4a35a0d9fa0281ac2c0137f93278
Dirk Hoffmann <hoffi.mail@gmail.com>
2021-07-13 00:06:28 +0200

    custom jq filter

--- filter_plugins/myFilters.py
+++ filter_plugins/myFilters.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
+import json
+import subprocess
+import yaml
+
+
+from ansible.module_utils._text import to_bytes, to_native, to_text
+from ansible.parsing.ajson import AnsibleJSONEncoder
+from ansible.parsing.yaml.dumper import AnsibleDumper
+
 class FilterModule(object):
     def filters(self):
         return {
             'a_filter': self.a_filter,
-            'another_filter': self.b_filter
+            'another_filter': self.b_filter,
+            'jq': self.jq_filter
         }
 
     def a_filter(self, a_variable):
@@ -13,3 +23,14 @@ class FilterModule(object):
     def b_filter(self, a_variable, another_variable, yet_another_variable):
         a_new_variable = a_variable + ' - ' + another_variable + ' - ' + yet_another_variable
         return a_new_variable
+
+    def jq_filter(self, jsonVar, jqCmd, jqOptions='', *args, **kw):
+        cmd = ['bash', '-c', "echo '{}' | jq {} '{}'".format(json.dumps(jsonVar, cls=AnsibleJSONEncoder), jqOptions, jqCmd)]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        return out.decode("utf-8")
+
+    # def jq_filter(self, a, indent=2, sort_keys=False, *args, **kw):
+    #     transformed = json.dumps(a, cls=AnsibleJSONEncoder, indent=indent, sort_keys=sort_keys, separators=(',', ': '), *args, **kw)
+    #     return transformed
