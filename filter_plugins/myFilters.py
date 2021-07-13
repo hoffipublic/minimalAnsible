4c07e15accba2a623d3432a2e5ea74e20027a699
HoffiMuc <hoffi.mail@gmail.com>
2021-07-13 12:21:30 +0200

    added jq_merge filter to merge two yaml variables via bash using jq

--- filter_plugins/myFilters.py
+++ filter_plugins/myFilters.py
@@ -3,17 +3,21 @@ import json
 import subprocess
 import yaml
 
+
 from ansible.errors import AnsibleError, AnsibleFilterError, AnsibleFilterTypeError
 from ansible.module_utils._text import to_bytes, to_native, to_text
 from ansible.parsing.ajson import AnsibleJSONEncoder
 from ansible.parsing.yaml.dumper import AnsibleDumper
+
+# more examples e.g. in ansible core filters: https://github.com/ansible/ansible/blob/devel/lib/ansible/plugins/filter/core.py
 
 class FilterModule(object):
     def filters(self):
         return {
             'a_filter': self.a_filter,
             'another_filter': self.b_filter,
-            'jq': self.jq_filter
+            'jq': self.jq_filter,
+            'jq_merge': self.jq_merge
         }
 
     def a_filter(self, a_variable):
@@ -38,6 +42,27 @@ class FilterModule(object):
             raise AnsibleFilterError(err.decode('utf-8'))
         return out.decode('utf-8')
 
-    # def jq_filter(self, a, indent=2, sort_keys=False, *args, **kw):
-    #     transformed = json.dumps(a, cls=AnsibleJSONEncoder, indent=indent, sort_keys=sort_keys, separators=(',', ': '), *args, **kw)
-    #     return transformed
+    def jq_merge(self, jsonVar1, jsonVar2, *args, **kw):
+        try:
+            subprocess.call(['jq', '--version'])
+        except FileNotFoundError:
+            raise AnsibleFilterError('jq not installed or available')
+
+        cmd = ['bash', '-c', """jq -s 'def deepmerge(a;b):
+  reduce b[] as $item (a;
+    reduce ($item | keys_unsorted[]) as $key (.;
+      $item[$key] as $val | ($val | type) as $type | .[$key] = if ($type == "object") then
+        deepmerge({}; [if .[$key] == null then {} else .[$key] end, $val])
+      elif ($type == "array") then
+        (.[$key] + $val | unique)
+      else
+        $val
+      end)
+    );
+  deepmerge({}; .)' <(echo '{}') <(echo '{}')""".format('{}', '{}', '{}', json.dumps(jsonVar1, cls=AnsibleJSONEncoder), json.dumps(jsonVar2, cls=AnsibleJSONEncoder))]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if err:
+            raise AnsibleFilterError(err.decode('utf-8'))
+        return out.decode('utf-8')
